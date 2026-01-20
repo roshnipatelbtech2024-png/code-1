17bedfa88c51976c244a08b5b0f683aadc6235ac
Roshni Patel <roshni.patel.btech2024@sitpune.edu.in>
2026-01-20 09:51:51 +0530

    Add files via upload

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,12 @@
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        int result = a * b;
+
+        System.out.println("Multiplication = " + result);
+    }
+}
+
---
+++ PrintBestFriendName.java
@@ -0,0 +1,7 @@
+public class PrintBestFriendName {
+
+    public static void main(String[] args) {
+        System.out.println("My best friend's name is: FriendName");
+    }
+}
+
